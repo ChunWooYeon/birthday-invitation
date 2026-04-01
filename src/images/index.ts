8a5968a134faf0a37d2b14c34ccc61e589ce9133
d <codewus@gmail.com>
2026-04-01 14:39:11 +0900

    [수정] 이미지 파일 수정

--- src/images/index.ts
+++ src/images/index.ts
@@ -7,18 +7,23 @@ import image5 from "./image5.png"
 import image6 from "./image6.png"
 import image7 from "./image7.png"
 import image8 from "./image8.png"
-import image9 from "./image9.png"
-import image10 from "./image10.png"
-import image11 from "./image11.png"
-import image12 from "./image12.png"
+import image9 from "./image9.jpg"
+import image10 from "./image10.jpg"
+import image11 from "./image11.jpg"
+import image12 from "./image12.jpg"
 import image13 from "./image13.png"
 import image14 from "./image14.png"
-import image15 from "./image15.png"
+import image15 from "./image15.jpg"
 import image16 from "./image16.png"
-import image17 from "./image17.png"
+import image17 from "./image17.jpg"
 import image18 from "./image18.png"
 import image19 from "./image19.png"
 import image20 from "./image20.png"
+import image21 from "./image21.png"
+import image22 from "./image22.png"
+import image23 from "./image23.png"
+import image24 from "./image24.png"
+import image25 from "./image25.png"
 
 export const COVER_IMAGE = coverImage
 
@@ -43,4 +48,9 @@ export const GALLERY_IMAGES = [
   image18,
   image19,
   image20,
+  image21,
+  image22,
+  image23,
+  image24,
+  image25,
 ]
